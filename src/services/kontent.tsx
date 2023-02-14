b84ad2e4cf70c43af7854e63b2036e181dd24fc9
Michael Berry <michaelb@kentico.com>
2023-02-14 14:26:13 -0500

    Adds kontent

---
+++ src/services/kontent.tsx
@@ -0,0 +1,49 @@
+import { createDeliveryClient, IContentItem, IContentItemElements } from "@kontent-ai/delivery-sdk";
+
+const deliveryClient = createDeliveryClient({
+    projectId: process.env.KONTENT_PROJECT_ID ?? ''
+});
+
+export async function getItemByCodename<T extends IContentItem<IContentItemElements>>(codename: string, depth: number) {
+    const response = await deliveryClient.item<T>(codename)
+        .depthParameter(depth)
+        .toPromise()
+
+    return response.data.item
+}
+
+export async function getItemByUrlSlug<T extends IContentItem<IContentItemElements>>(element: string, slug: string, depth: number) {
+    const response = await deliveryClient.items<T>()
+        .equalsFilter(element, slug)
+        .depthParameter(depth)
+        .toPromise()
+
+    return response.data.items[0]
+}
+
+export async function getItems<T extends IContentItem<IContentItemElements>>(depth: number) {
+    const response = await deliveryClient.items<T>()
+        .depthParameter(depth)
+        .toPromise()
+
+    return response.data.items
+}
+
+export async function getItemsOfType<T extends IContentItem<IContentItemElements>>(type: string, depth: number) {
+    const response = await deliveryClient.items<T>()
+        .type(type)
+        .depthParameter(depth)
+        .toPromise()
+
+    return response.data.items
+}
+
+export async function getNavigationItems<T extends IContentItem<IContentItemElements>>(homepageCodename: string, subpagesCodename: string, depth: number) {
+    const response = await deliveryClient.item<T>(homepageCodename)
+        .depthParameter(depth)
+        .elementsParameter([subpagesCodename])
+        .toPromise()
+
+    return response.data.item
+}
+
