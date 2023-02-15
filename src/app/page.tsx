ce1345257d2323830a3e85172567cb8827091f48
Michael Berry <michaelb@kentico.com>
2023-02-15 13:34:09 -0500

    Adds composability to homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,20 +2,37 @@ import { Homepage } from '@/models/content-types/Homepage'
 import { contentTypes } from '@/models'
 import { getItemByCodename } from '@/services/kontent'
 import styles from './page.module.css'
+import ComposableLayout from '@/components/ComposableLayout'
 
 export default async function Home() {
   const home = await getPageData()
+  const featured = home.elements.featuredContent.linkedItems
+  const content = home.elements.content.linkedItems
   return (
     <main className={styles.main}>
-      <div className={styles.description}>
-        {home.elements.title.value}
+      <div>
+        <div>
+          {home.elements.title.value}
+        </div>
+        <div>
+          <h2>Featured</h2>
+          {featured.length > 0 &&
+            featured.map(item => (<ComposableLayout item={item} key={item.system.id} />))
+          }
+        </div>
+        <div>
+          <h2>Regular Content:</h2>
+          {content.length > 0 &&
+            content.map((item) => (<ComposableLayout item={item} key={item.system.id} />))
+          }
+        </div>
       </div>
     </main>
   )
 }
 
-async function getPageData(){
+async function getPageData() {
   const homepageCodename = contentTypes.homepage.codename
-  const item = await getItemByCodename<Homepage>(homepageCodename, 1)
+  const item = await getItemByCodename<Homepage>(homepageCodename, 2)
   return item
 }
